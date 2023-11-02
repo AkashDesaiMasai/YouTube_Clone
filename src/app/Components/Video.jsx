2bfad8ae0dcc89b4503de788453381f4d1eb6071
Akash Desai <132215036+AakashDesai3105@users.noreply.github.com>
2023-11-02 15:57:14 +0530

    changed string length

--- src/app/Components/Video.jsx
+++ src/app/Components/Video.jsx
@@ -7,15 +7,16 @@ const Video = ({id:{videoId}, snippet,}) => {
    
  const demoVideoUrl="/video/GDa8kZLNhJ4";
   return (
-    <div className="w-64 border-white rounded-lg m-5 h-84" >
+    <div className="w-64 border-white rounded-lg m-5 h-84 max-h-96" >
       <Link href={videoId? `video/${videoId}`: demoVideoUrl}>
       <Image
         src={snippet.thumbnails.high.url}
         width={360}
         height={480}
+        className="max-h-48"
         alt={snippet.title}
         />
-      <h2>{snippet.title||"Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI'"}</h2>
+      <h2 >{snippet.title.substring(0,75)||"Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI'"}</h2>
       <h1 className="text-l text-gray-500">{snippet.channelTitle.substring(0,25)}</h1>
       <p className="text-gray-500 text-s">{Math.floor(Math.random() * 100)}k views</p>
         </Link>
